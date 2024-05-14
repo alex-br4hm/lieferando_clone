1c085d2efa212ef0c79927d288c84887be6d9b5e
br4hm <haehnlein.alexander@gmx.de>
2024-05-14 20:40:12 +0200

    build: customer basket functionallity

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 const dishesFavouriteContainer = document.getElementById("dishesFavouriteContainer");
 const dishesAllContainer = document.getElementById("dishesAllContainer");
+const shoppingCartContainer = document.getElementById("shoppingCartContainer");
+const shoppingCartProductContainer = document.getElementById("shoppingCartProductContainer");
+const costsContainer = document.getElementById("costsContainer");
 renderFavouriteProducts();
 renderAllProducts();
 
@@ -19,14 +22,15 @@ function renderFavouriteProducts() {
                    ${product.productName}<img src="./src/img/info_icon.png" alt="information icon" />
                 </h3>
                 <div class="counter-icon">
-                   <img src="./src/img/add_icon.png" alt="add icon" class="add-icon" />
+                   <img src="./src/img/add_icon.png" alt="add icon" class="add-icon" onclick="addToShoppingCart('${product.productName}', ${product.productPrice})"/>
                 </div>
              </div>
              <div class="dishes-single-description">
-                ${product.productDescription ? product.productDescription : ""}
+                ${product.productDescription}
              </div>
-             <div class="dishes-single-price"><span id="dishPrice">${product.productPrice.toFixed(2)}</span>€</div>
-          </div>`;
+             <div class="dishes-single-price"><span id="dishPrice">${product.productPrice}</span>€</div>
+          </div>
+          `;
       }
    }
 }
@@ -48,14 +52,124 @@ function renderAllProducts() {
                     ${product.productName}<img src="./src/img/info_icon.png" alt="information icon" />
                  </h3>
                  <div class="counter-icon">
-                    <img src="./src/img/add_icon.png" alt="add icon" class="add-icon" />
+                    <img src="./src/img/add_icon.png" alt="add icon" class="add-icon" onclick="addToShoppingCart('${
+                       product.productName
+                    }', ${product.productPrice})"/>
                  </div>
               </div>
               <div class="dishes-single-description">
                  ${product.productDescription ? product.productDescription : ""}
               </div>
-              <div class="dishes-single-price"><span id="dishPrice">${product.productPrice.toFixed(2)}</span>€</div>
+              <div class="dishes-single-price"><span id="dishPrice">${product.productPrice}</span>€</div>
            </div>`;
       }
    }
 }
+
+function addToShoppingCart(productName, productPrice) {
+   let productPriceFloat = parseFloat(productPrice).toFixed(2);
+   let itemAmount = 1;
+   newShoppingCartProduct = {
+      "itemName": `${productName}`,
+      "itemPrice": `${productPriceFloat}`,
+      "itemAmount": `${itemAmount}`,
+   };
+
+   shoppingCartProducts.push(newShoppingCartProduct);
+   renderShoppingCartItem();
+}
+
+function renderShoppingCartItem() {
+   calculateCosts();
+   shoppingCartProductContainer.innerHTML = "";
+   for (let i = 0; i < shoppingCartProducts.length; i++) {
+      shoppingCartProductContainer.innerHTML += `
+           <div class="selected-dish-head">
+              <div class="selected-dish-head-left">
+                 <div class="selected-dish-count">1</div>
+                 <div class="selected-dish-name">${shoppingCartProducts[i].itemName}</div>
+              </div>
+              <div class="selected-dish-price"><span>${shoppingCartProducts[i].itemPrice}</span>€</div>
+           </div>
+           <div class="selected-dish-bottom">
+              <div class="selected-dish-addcomment">Anmerkung hinzufügen</div>
+              <div class="selected-dish-left-counter">
+                 <div class="selected-dish-counter-icon">
+                    <img src="./src/img/remove_icon.png" alt="remove icon" onclick="removeItemFromShoppingCart(${i})"/>
+                 </div>
+                 <div class="selected-dish-price"><span>${shoppingCartProducts[i].itemAmount}</span></div>
+                 <div class="selected-dish-counter-icon"><img src="./src/img/add_icon.png" alt="add icon" onclick="increaseQuantityFromItem(${i})"/></div>
+              </div>
+           </div>
+        `;
+   }
+   calculateCosts();
+}
+
+function increaseQuantityFromItem(i) {
+   shoppingCartProducts[i].itemAmount = parseFloat(shoppingCartProducts[i].itemAmount) + 1;
+   console.table(shoppingCartProducts);
+   renderShoppingCartItem();
+}
+
+function removeItemFromShoppingCart(i) {
+   if (shoppingCartProducts[i].itemAmount > 1) {
+      shoppingCartProducts[i].itemAmount = parseFloat(shoppingCartProducts[i].itemAmount) - 1;
+      renderShoppingCartItem();
+   } else {
+      shoppingCartProducts.splice(i, 1);
+      renderShoppingCartItem();
+   }
+}
+
+function calculateCosts() {
+   let subTotal = 0;
+   let costsTotal = 0;
+   for (let i = 0; i < shoppingCartProducts.length; i++) {
+      subTotal += parseFloat(shoppingCartProducts[i].itemAmount) * parseFloat(shoppingCartProducts[i].itemPrice);
+      costsTotal = subTotal + 0.49 + 0.99;
+   }
+   subTotal = parseFloat(subTotal.toFixed(2));
+   costsTotal = parseFloat(costsTotal.toFixed(2));
+
+   renderCosts(subTotal, costsTotal);
+}
+
+function renderCosts(subTotal, costsTotal) {
+   costsContainer.innerHTML = "";
+   const formattedSubTotal = subTotal.toFixed(2);
+   const formattedCostsTotal = costsTotal.toFixed(2);
+
+   costsContainer.innerHTML = `
+       <div class="costs-sum-wrapper costs-wrapper">
+          <div>Zwischensumme</div>
+          <div class="costs-sum"><span>${formattedSubTotal}</span>€</div>
+       </div>
+       <div class="costs-delivery-wrapper costs-wrapper">
+          <div>Lieferkosten</div>
+          <div class="costs-sum"><span>0.49</span>€</div>
+       </div>
+       <div class="costs-service-wrapper costs-wrapper">
+          <div>Servicegebühr <img src="./src/img/info_icon.png" alt="info_icon" /></div>
+          <div class="costs-sum"><span>0.99</span>€</div>
+       </div>
+       <div class="costs-all-wrapper costs-wrapper">
+          <div>Gesamt</div>
+          <div class="costs-sum"><span>${formattedCostsTotal}</span>€</div>
+       </div>
+       <div class="pay-button">
+          <div>Bezahlen</div>
+          <div class="costs-pay-button">(<span>${formattedCostsTotal}</span>€)</div>
+       </div>
+    `;
+}
+
+window.addEventListener("scroll", function () {
+   let scrollPosition = window.scrollY;
+
+   if (scrollPosition < 60) {
+      shoppingCartContainer.style.position = "absolute";
+   } else {
+      shoppingCartContainer.style.position = "fixed";
+   }
+});
--- src/js/appData.js
+++ src/js/appData.js
@@ -5,17 +5,17 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Cheeseburger",
-            "productPrice": 9.6,
+            "productPrice": "9.60",
             "productDescription":
                "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Irish Cheddar und muttimade Burgersauce",
          },
          {
             "productName": "Kloßpommes",
-            "productPrice": 5.9,
+            "productPrice": "5.90",
          },
          {
             "productName": "Fritten Chili Cheese",
-            "productPrice": 5.9,
+            "productPrice": "5.90",
             "productDescription": "mit Cheddarsauce und Jalapenos",
          },
       ],
@@ -26,65 +26,65 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Hamburger",
-            "productPrice": 8.6,
+            "productPrice": "8.60",
             "productDescription":
                "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce",
          },
          {
             "productName": "Cheeseburger",
-            "productPrice": 9.6,
+            "productPrice": "9.60",
             "productDescription":
                "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Irish Cheddar und muttimade Burgersauce",
          },
          {
             "productName": "Bacon Burger",
-            "productPrice": 9.9,
+            "productPrice": "9.90",
             "productDescription":
                "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Bacon und muttimade Burgersauce",
          },
          {
             "productName": "Thüringen Burger",
-            "productPrice": 7.9,
+            "productPrice": "7.90",
             "productDescription":
                "mit Bratwurstpatty, Krautsalat oder Rotkraut, Salat, roten Zwiebel und muttimade Thüringen-Burgersauce",
          },
          {
             "productName": "Jumboburger",
-            "productPrice": 15.4,
+            "productPrice": "15.40",
             "productDescription":
                "mit doppeltem 180g Rindfleisch, doppeltem Irish Cheddar, doppeltem Bacon, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce",
          },
          {
             "productName": "Blue Cheeseburger",
-            "productPrice": 9.9,
+            "productPrice": "9.90",
             "productDescription":
                "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Gorgonzola und muttimade Burgersauce",
          },
          {
             "productName": "Moving Mountain Cheeseburger (vegan oder vegetarisch)",
-            "productPrice": 9.9,
+            "productPrice": "9.90",
             "productDescription":
                "mit Moving Mountain Patty, Irish Cheddar, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce. Bitte wählt zwischen vegan oder vegetarisch.",
          },
          {
             "productName": "Pulled Beef Burger",
-            "productPrice": 9.9,
+            "productPrice": "9.90",
             "productDescription": "mit Pulled Beef, Krautsalat und Barbecuesauce",
          },
          {
             "productName": "Falafel Cheeseburger (vegan oder vegetarisch)",
-            "productPrice": 8.9,
+            "productPrice": "8.90",
             "productDescription":
                "mit Falafelpatty, Irish Cheddar, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce. Bitte wählt zwischen vegan oder vegetarisch.",
          },
          {
             "productName": "Pulled Chicken Burger",
-            "productPrice": 9.4,
+            "productPrice": "9.40",
             "productDescription": "mit Pulled Chicken und muttimade Mango Chutney",
          },
          {
             "productName": "Low Carb Cheeseburger",
-            "productPrice": 8.4,
+            "productPrice": "8.40",
             "productDescription":
                "ohne Brötchen, mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Irish Cheddar und muttimade Burgersauce",
          },
@@ -96,31 +96,31 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Fritten Pur",
-            "productPrice": 3.9,
+            "productPrice": "3.90",
          },
          {
             "productName": "Fritten Chili Cheese",
-            "productPrice": 5.9,
+            "productPrice": "5.90",
             "productDescription": "mit Cheddarsauce und Jalapenos",
          },
          {
             "productName": "Fritten Avocado",
-            "productPrice": 5.9,
+            "productPrice": "5.90",
             "productDescription": "mit Avocado, Sour Cream und Tomatenwürfeln",
          },
          {
             "productName": "Fritten Pulled Beef",
-            "productPrice": 7.9,
+            "productPrice": "7.90",
             "productDescription": "mit Pulled Beef, Krautsalat und Barbecuesauce",
          },
          {
             "productName": "Fritten Pulled Chicken",
-            "productPrice": 7.4,
+            "productPrice": "7.40",
             "productDescription": "Pulled Chicken, Krautsalat und Mango Chutney",
          },
          {
             "productName": "Fritten Parmesan",
-            "productPrice": 4.9,
+            "productPrice": "4.90",
          },
       ],
    },
@@ -130,22 +130,22 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Buddha Bowls Klassik (vegan)",
-            "productPrice": 10.9,
+            "productPrice": "10.90",
             "productDescription":
                "mit Quinoa, Edamame, Salatmischung, Kichererbsen, Avocadocreme, Karotten, Mais, Gurken, Tomaten, Granatapfelkernen und Sesam",
          },
          {
             "productName": "Buddha Bowls Orientalisch (vegan)",
-            "productPrice": 11.9,
+            "productPrice": "11.90",
             "productDescription": "mit Falafel-Bällchen",
          },
          {
             "productName": "Buddha Bowls Chicken",
-            "productPrice": 11.9,
+            "productPrice": "11.90",
          },
          {
             "productName": "Buddha Bowls Aloha (vegan)",
-            "productPrice": 15.9,
+            "productPrice": "15.90",
             "productDescription": "mit veganem Lachs- und Thunfisch-Sashimi",
          },
       ],
@@ -156,35 +156,35 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Rindergulasch mit Heichelheimer Klößen und Rotkraut",
-            "productPrice": 11.9,
+            "productPrice": "11.90",
          },
          {
             "productName": "Rindergulasch mit Kloßpommes (glutenfrei) und Rotkraut",
-            "productPrice": 11.9,
+            "productPrice": "11.90",
          },
          {
             "productName": "1/2 Broiler (Hähnchen) mit Fritten",
-            "productPrice": 11.5,
+            "productPrice": "11.50",
             "productDescription": "frisch gegrillter halber Broiler mit frischen Steakhaus Fritten",
          },
          {
             "productName": "Hähnchenschenkel mit Fritten",
-            "productPrice": 7.5,
+            "productPrice": "7.50",
             "productDescription": "frisch gegrillter Hähnchenschenkel mit Steakhaus Fritten",
          },
          {
             "productName": "Hähnchenschenkel mit hausgemachten Kartoffelsalat von Mutti",
-            "productPrice": 7.5,
+            "productPrice": "7.50",
             "productDescription": "frisch gegrillter Hähnchenschenkel mit hausgemachten Kartoffelsalat von Mutti",
          },
          {
             "productName": "1/2 Broiler (Hähnchen) mit hausgemachten Kartoffelsalat",
-            "productPrice": 10.9,
+            "productPrice": "10.90",
             "productDescription": "frisch gegrillter halber Broiler mit hausgemachten Kartoffelsalat von Mutti",
          },
          {
             "productName": "5 Heichelheimer Kartoffelpuffer mit Apfelmus",
-            "productPrice": 6.9,
+            "productPrice": "6.90",
             "productDescription": "5 knusprige Heichelheimer Kartoffelpuffer mit Apfelmus",
          },
       ],
@@ -195,50 +195,50 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Coca-Cola 0,4l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
             "productDescription": "Enthält Koffein (10,00 mg/100 ml)",
          },
          {
             "productName": "Coca-Cola Zero Sugar 0,4l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
             "productDescription": "Enthält Koffein (10,00 mg/100 ml)",
          },
          {
             "productName": "Fanta Orange 0,4l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
          },
          {
             "productName": "Mezzo Mix 0,4l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
             "productDescription": "Enthält Koffein (6,00 mg/100 ml)",
          },
          {
             "productName": "Sprite 0,4l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
          },
          {
             "productName": "Fuze Tea Schwarzer Tee Pfirsich 0,3l (Mehrweg)",
-            "productPrice": 2.55,
+            "productPrice": "2.55",
          },
          {
             "productName": "Fuze Tea Schwarzer Tee Zitrone 0,3l (Mehrweg)",
-            "productPrice": 2.55,
+            "productPrice": "2.55",
          },
          {
             "productName": "ViO Schorle Apfel 0,3l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
          },
          {
             "productName": "ViO Schorle Schwarze Johannisbeere 0,3l (MEHRWEG)",
-            "productPrice": 2.65,
+            "productPrice": "2.65",
          },
          {
             "productName": "Mineralwasser Sprudel 0,25l",
-            "productPrice": 2.35,
+            "productPrice": "2.35",
          },
          {
             "productName": "Mineralwasser Still 0,25l",
-            "productPrice": 2.35,
+            "productPrice": "2.35",
          },
       ],
    },
@@ -248,11 +248,11 @@ let products = [
       "categoryProducts": [
          {
             "productName": "Bayreuther Hell 0,33l",
-            "productPrice": 2.87,
+            "productPrice": "2.87",
             "ageRestriction": "18 plus",
          },
       ],
    },
 ];
 
-console.table(products);
+let shoppingCartProducts = [];
