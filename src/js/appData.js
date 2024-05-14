76345c1edc2b101c2d10dcb80a07c05d1c1459f3
br4hm <haehnlein.alexander@gmx.de>
2024-05-14 14:31:58 +0200

    build: product data

---
+++ src/js/appData.js
@@ -0,0 +1,254 @@
+let products = [
+   {
+      "category": "favourite",
+      "headline": "Beliebt",
+      "imgSrc": "",
+      "categoryProducts": [
+         {
+            "productName": "Cheeseburger",
+            "productPrice": 9.6,
+            "productDescription":
+               "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Irish Cheddar und muttimade Burgersauce",
+         },
+         {
+            "productName": "Kloßpommes",
+            "productPrice": 5.9,
+         },
+         {
+            "productName": "Fritten Chili Cheese",
+            "productPrice": 5.9,
+            "productDescription": "mit Cheddarsauce und Jalapenos",
+         },
+      ],
+   },
+   {
+      "category": "Burger",
+      "imgSrc": "",
+      "categoryProducts": [
+         {
+            "productName": "Hamburger",
+            "productPrice": 8.6,
+            "productDescription":
+               "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce",
+         },
+         {
+            "productName": "Cheeseburger",
+            "productPrice": 9.6,
+            "productDescription":
+               "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Irish Cheddar und muttimade Burgersauce",
+         },
+         {
+            "productName": "Bacon Burger",
+            "productPrice": 9.9,
+            "productDescription":
+               "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Bacon und muttimade Burgersauce",
+         },
+         {
+            "productName": "Thüringen Burger",
+            "productPrice": 7.9,
+            "productDescription":
+               "mit Bratwurstpatty, Krautsalat oder Rotkraut, Salat, roten Zwiebel und muttimade Thüringen-Burgersauce",
+         },
+         {
+            "productName": "Jumboburger",
+            "productPrice": 15.4,
+            "productDescription":
+               "mit doppeltem 180g Rindfleisch, doppeltem Irish Cheddar, doppeltem Bacon, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce",
+         },
+         {
+            "productName": "Blue Cheeseburger",
+            "productPrice": 9.9,
+            "productDescription":
+               "mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Gorgonzola und muttimade Burgersauce",
+         },
+         {
+            "productName": "Moving Mountain Cheeseburger (vegan oder vegetarisch)",
+            "productPrice": 9.9,
+            "productDescription":
+               "mit Moving Mountain Patty, Irish Cheddar, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce. Bitte wählt zwischen vegan oder vegetarisch.",
+         },
+         {
+            "productName": "Pulled Beef Burger",
+            "productPrice": 9.9,
+            "productDescription": "mit Pulled Beef, Krautsalat und Barbecuesauce",
+         },
+         {
+            "productName": "Falafel Cheeseburger (vegan oder vegetarisch)",
+            "productPrice": 8.9,
+            "productDescription":
+               "mit Falafelpatty, Irish Cheddar, Salat, Tomaten, roten Zwiebeln, Gurken und muttimade Burgersauce. Bitte wählt zwischen vegan oder vegetarisch.",
+         },
+         {
+            "productName": "Pulled Chicken Burger",
+            "productPrice": 9.4,
+            "productDescription": "mit Pulled Chicken und muttimade Mango Chutney",
+         },
+         {
+            "productName": "Low Carb Cheeseburger",
+            "productPrice": 8.4,
+            "productDescription":
+               "ohne Brötchen, mit 180g Rindfleisch, Salat, Tomaten, roten Zwiebeln, Gurken, Irish Cheddar und muttimade Burgersauce",
+         },
+      ],
+   },
+   {
+      "category": "Fritten",
+      "categoryProducts": [
+         {
+            "productName": "Fritten Pur",
+            "productPrice": 3.9,
+         },
+         {
+            "productName": "Fritten Chili Cheese",
+            "productPrice": 5.9,
+            "productDescription": "mit Cheddarsauce und Jalapenos",
+         },
+         {
+            "productName": "Fritten Avocado",
+            "productPrice": 5.9,
+            "productDescription": "mit Avocado, Sour Cream und Tomatenwürfeln",
+         },
+         {
+            "productName": "Fritten Pulled Beef",
+            "productPrice": 7.9,
+            "productDescription": "mit Pulled Beef, Krautsalat und Barbecuesauce",
+         },
+         {
+            "productName": "Fritten Pulled Chicken",
+            "productPrice": 7.4,
+            "productDescription": "Pulled Chicken, Krautsalat und Mango Chutney",
+         },
+         {
+            "productName": "Fritten Parmesan",
+            "productPrice": 4.9,
+         },
+      ],
+   },
+   {
+      "category": "Buddha Bowls",
+      "categoryProducts": [
+         {
+            "productName": "Buddha Bowls Klassik (vegan)",
+            "productPrice": 10.9,
+            "productDescription":
+               "mit Quinoa, Edamame, Salatmischung, Kichererbsen, Avocadocreme, Karotten, Mais, Gurken, Tomaten, Granatapfelkernen und Sesam",
+         },
+         {
+            "productName": "Buddha Bowls Orientalisch (vegan)",
+            "productPrice": 11.9,
+            "productDescription": "mit Falafel-Bällchen",
+         },
+         {
+            "productName": "Buddha Bowls Chicken",
+            "productPrice": 11.9,
+         },
+         {
+            "productName": "Buddha Bowls Aloha (vegan)",
+            "productPrice": 15.9,
+            "productDescription": "mit veganem Lachs- und Thunfisch-Sashimi",
+         },
+      ],
+   },
+   {
+      "category": "Hähnchen, Gulasch & Co.",
+      "categoryProducts": [
+         {
+            "productName": "Rindergulasch mit Heichelheimer Klößen und Rotkraut",
+            "productPrice": 11.9,
+         },
+         {
+            "productName": "Rindergulasch mit Kloßpommes (glutenfrei) und Rotkraut",
+            "productPrice": 11.9,
+         },
+         {
+            "productName": "1/2 Broiler (Hähnchen) mit Fritten",
+            "productPrice": 11.5,
+            "productDescription": "frisch gegrillter halber Broiler mit frischen Steakhaus Fritten",
+         },
+         {
+            "productName": "Hähnchenschenkel mit Fritten",
+            "productPrice": 7.5,
+            "productDescription": "frisch gegrillter Hähnchenschenkel mit Steakhaus Fritten",
+         },
+         {
+            "productName": "Hähnchenschenkel mit hausgemachten Kartoffelsalat von Mutti",
+            "productPrice": 7.5,
+            "productDescription": "frisch gegrillter Hähnchenschenkel mit hausgemachten Kartoffelsalat von Mutti",
+         },
+         {
+            "productName": "1/2 Broiler (Hähnchen) mit hausgemachten Kartoffelsalat",
+            "productPrice": 10.9,
+            "productDescription": "frisch gegrillter halber Broiler mit hausgemachten Kartoffelsalat von Mutti",
+         },
+         {
+            "productName": "5 Heichelheimer Kartoffelpuffer mit Apfelmus",
+            "productPrice": 6.9,
+            "productDescription": "5 knusprige Heichelheimer Kartoffelpuffer mit Apfelmus",
+         },
+      ],
+   },
+   {
+      "category": "Getränke alkoholfrei",
+      "categoryProducts": [
+         {
+            "productName": "Coca-Cola 0,4l (MEHRWEG)",
+            "productPrice": 2.65,
+            "productDescription": "Enthält Koffein (10,00 mg/100 ml)",
+         },
+         {
+            "productName": "Coca-Cola Zero Sugar 0,4l (MEHRWEG)",
+            "productPrice": 2.65,
+            "productDescription": "Enthält Koffein (10,00 mg/100 ml)",
+         },
+         {
+            "productName": "Fanta Orange 0,4l (MEHRWEG)",
+            "productPrice": 2.65,
+         },
+         {
+            "productName": "Mezzo Mix 0,4l (MEHRWEG)",
+            "productPrice": 2.65,
+            "productDescription": "Enthält Koffein (6,00 mg/100 ml)",
+         },
+         {
+            "productName": "Sprite 0,4l (MEHRWEG)",
+            "productPrice": 2.65,
+         },
+         {
+            "productName": "Fuze Tea Schwarzer Tee Pfirsich 0,3l (Mehrweg)",
+            "productPrice": 2.55,
+         },
+         {
+            "productName": "Fuze Tea Schwarzer Tee Zitrone 0,3l (Mehrweg)",
+            "productPrice": 2.55,
+         },
+         {
+            "productName": "ViO Schorle Apfel 0,3l (MEHRWEG)",
+            "productPrice": 2.65,
+         },
+         {
+            "productName": "ViO Schorle Schwarze Johannisbeere 0,3l (MEHRWEG)",
+            "productPrice": 2.65,
+         },
+         {
+            "productName": "Mineralwasser Sprudel 0,25l",
+            "productPrice": 2.35,
+         },
+         {
+            "productName": "Mineralwasser Still 0,25l",
+            "productPrice": 2.35,
+         },
+      ],
+   },
+   {
+      "category": "Getränke mit Alkohol",
+      "categoryProducts": [
+         {
+            "productName": "Bayreuther Hell 0,33l",
+            "productPrice": 2.87,
+            "ageRestriction": "18 plus",
+         },
+      ],
+   },
+];
+
+console.table(products);
