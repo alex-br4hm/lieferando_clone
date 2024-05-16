dedda3cf95387d68f2ee7efd54f21cdd0e44db95
br4hm <haehnlein.alexander@gmx.de>
2024-05-16 13:57:46 +0200

    refactor: HTML outsource

--- src/js/app.js
+++ src/js/app.js
@@ -16,59 +16,18 @@ renderShoppingCartBtnMobile();
 function renderFavouriteProducts() {
    dishesFavouriteContainer.innerHTML = "";
    for (let i = 0; i < 1; i++) {
-      dishesFavouriteContainer.innerHTML += ` 
-      <h2 class="headline-favourits-icon" id="favourits">${products[i].category}
-      <img src="./src/img/heart_liked.png" alt="heart icon" /></h2>`;
-      let categoryProducts = products[i].categoryProducts;
-      for (let j = 0; j < categoryProducts.length; j++) {
-         let product = categoryProducts[j];
-         dishesFavouriteContainer.innerHTML += `
-          <div class="dishes-single-wrapper">
-             <div class="dishes-single-head-wrapper">
-                <h3 class="dishes-single-headline">
-                   ${product.productName}<img src="./src/img/info_icon.png" alt="information icon" />
-                </h3>
-                <div class="counter-icon" onclick="addToShoppingCart('${product.productName}', ${product.productPrice})">
-                   <img src="./src/img/add_icon.png" alt="add icon" class="add-icon" />
-                </div>
-             </div>
-             <div class="dishes-single-description">
-                ${product.productDescription}
-             </div>
-             <div class="dishes-single-price"><span id="dishPrice">${product.productPrice}</span>€</div>
-          </div>
-          `;
-      }
+      renderFavouriteProductsHTML(products, i);
    }
 }
 
 function renderAllProducts() {
    dishesAllContainer.innerHTML = "";
    for (let i = 1; i < products.length; i++) {
-      dishesAllContainer.innerHTML += ` 
-       <img src="${products[i].imgSrc}.png" alt="" id="${products[i].id}"/>
-       <h2 class="headline-favourits-icon">${products[i].category}
-       <img src="./src/img/heart_liked.png" alt="heart icon" /></h2>`;
+      renderAllProductsHeadHTML(products, i);
       let categoryProducts = products[i].categoryProducts;
       for (let j = 0; j < categoryProducts.length; j++) {
          let product = categoryProducts[j];
-         dishesAllContainer.innerHTML += `
-           <div class="dishes-single-wrapper">
-              <div class="dishes-single-head-wrapper">
-                 <h3 class="dishes-single-headline">
-                    ${product.productName}<img src="./src/img/info_icon.png" alt="information icon" />
-                 </h3>
-                 <div class="counter-icon" onclick="addToShoppingCart('${product.productName}', ${
-            product.productPrice
-         })">
-                    <img src="./src/img/add_icon.png" alt="add icon" class="add-icon" />
-                 </div>
-              </div>
-              <div class="dishes-single-description">
-                 ${product.productDescription ? product.productDescription : ""}
-              </div>
-              <div class="dishes-single-price"><span id="dishPrice">${product.productPrice}</span>€</div>
-           </div>`;
+         renderAllProductsContentHTML(product);
       }
    }
 }
@@ -108,26 +67,7 @@ function renderShoppingCart() {
 
    shoppingCartProductContainer.innerHTML = "";
    for (let i = 0; i < shoppingCartProducts.length; i++) {
-      shoppingCartProductContainer.innerHTML += `
-           <div class="selected-dish-head">
-           
-              <div class="selected-dish-head-left">
-                 <div class="selected-dish-count">${shoppingCartProducts[i].itemAmount}</div>
-                 <div class="selected-dish-name">${shoppingCartProducts[i].itemName}</div>
-              </div>
-              <div class="selected-dish-price"><span>${shoppingCartProducts[i].itemPrice}</span>€</div>
-           </div>
-           <div class="selected-dish-bottom">
-              <div class="selected-dish-addcomment">Anmerkung hinzufügen</div>
-              <div class="selected-dish-left-counter">
-                 <div class="selected-dish-counter-icon">
-                    <img src="./src/img/remove_icon.png" alt="remove icon" onclick="removeItemFromShoppingCart(${i})"/>
-                 </div>
-                 <div class="selected-dish-price"><span>${shoppingCartProducts[i].itemAmount}</span></div>
-                 <div class="selected-dish-counter-icon"><img src="./src/img/add_icon.png" alt="add icon" onclick="increaseQuantityFromItem(${i})"/></div>
-              </div>
-           </div>
-        `;
+      renderShoppingCartHTML(i);
    }
 }
 
@@ -144,19 +84,14 @@ function shoppingCartStopOverloadPrevention() {
 }
 
 function shoppingCartEmptyStandard() {
-   costsContainer.innerHTML = `
-   <div class="empty-shopping-cart">
-   <img src="./src/img/shopping_bag.png" alt="shopping bag icon" />
-   <h2>Fülle deinen Warenkorb</h2>
-   <div>Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle dein Essen.</div>
-   </div>
-   `;
+   shoppingCartEmptyStandardHTML();
 }
 
 function increaseQuantityFromItem(i) {
    shoppingCartProducts[i].itemAmount = parseFloat(shoppingCartProducts[i].itemAmount) + 1;
    console.table(shoppingCartProducts);
    renderShoppingCart();
+   renderShoppingCartMobile();
 }
 
 function removeItemFromShoppingCart(i) {
@@ -189,29 +124,7 @@ function renderCosts(subTotal, costsTotal) {
    costsContainer.innerHTML = "";
    const formattedSubTotal = subTotal.toFixed(2);
    const formattedCostsTotal = costsTotal.toFixed(2);
-
-   costsContainer.innerHTML = `
-       <div class="costs-sum-wrapper costs-wrapper">
-          <div>Zwischensumme</div>
-          <div class="costs-sum"><span>${formattedSubTotal}</span>€</div>
-       </div>
-       <div class="costs-delivery-wrapper costs-wrapper">
-          <div>Lieferkosten</div>
-          <div class="costs-sum"><span>0.49</span>€</div>
-       </div>
-       <div class="costs-service-wrapper costs-wrapper">
-          <div>Servicegebühr <img src="./src/img/info_icon.png" alt="info_icon" /></div>
-          <div class="costs-sum"><span>0.99</span>€</div>
-       </div>
-       <div class="costs-all-wrapper costs-wrapper">
-          <div>Gesamt</div>
-          <div class="costs-sum"><span>${formattedCostsTotal}</span>€</div>
-       </div>
-       <div class="pay-button" onclick="sendOrderSucces()">
-          <div>Bezahlen</div>
-          <div class="costs-pay-button">(<span>${formattedCostsTotal}</span>€)</div>
-       </div>
-    `;
+   renderCostsHTML(formattedSubTotal, formattedCostsTotal);
 
    renderShoppingCartBtnMobile(formattedCostsTotal);
 }
@@ -220,29 +133,7 @@ function renderCostsMobile(subTotal, costsTotal) {
    costsContainerMobile.innerHTML = "";
    const formattedSubTotal = subTotal.toFixed(2);
    const formattedCostsTotal = costsTotal.toFixed(2);
-
-   costsContainerMobile.innerHTML = `
-       <div class="costs-sum-wrapper costs-wrapper">
-          <div>Zwischensumme</div>
-          <div class="costs-sum"><span>${formattedSubTotal}</span>€</div>
-       </div>
-       <div class="costs-delivery-wrapper costs-wrapper">
-          <div>Lieferkosten</div>
-          <div class="costs-sum"><span>0.49</span>€</div>
-       </div>
-       <div class="costs-service-wrapper costs-wrapper">
-          <div>Servicegebühr <img src="./src/img/info_icon.png" alt="info_icon" /></div>
-          <div class="costs-sum"><span>0.99</span>€</div>
-       </div>
-       <div class="costs-all-wrapper costs-wrapper">
-          <div>Gesamt</div>
-          <div class="costs-sum"><span>${formattedCostsTotal}</span>€</div>
-       </div>
-       <div class="pay-button" onclick="sendOrderSucces()">
-          <div>Bezahlen</div>
-          <div class="costs-pay-button">(<span>${formattedCostsTotal}</span>€)</div>
-       </div>
-    `;
+   renderCostsMobileHTML(formattedSubTotal, formattedCostsTotal);
 
    renderShoppingCartBtnMobile(formattedCostsTotal);
 }
@@ -264,15 +155,7 @@ function renderShoppingCartBtnMobile(formattedCostsTotal) {
    if (shoppingCartProducts.length === 0) {
       formattedCostsTotal = "0,00";
    }
-   shoppingCartBtnContainer.innerHTML = `
-   <div class="shopping-cart-btn-mobile pay-button" onclick="openShoppingCartMobile()">
-            <div class="shopping-bag-counter">
-               <img src="./src/img/shopping_bag.png" alt="" />
-               <span>${shoppingCartProducts.length}</span>
-            </div>
-            Warenkorb<span>(${formattedCostsTotal}€)</span>
-         </div>
-   `;
+   renderShoppingCartBtnMobileHTML(formattedCostsTotal);
 }
 
 function renderShoppingCartMobile() {
@@ -285,26 +168,7 @@ function renderShoppingCartMobile() {
 
    shoppingCartProductContainerMobile.innerHTML = "";
    for (let i = 0; i < shoppingCartProducts.length; i++) {
-      shoppingCartProductContainerMobile.innerHTML += `
-           <div class="selected-dish-head">
-           
-              <div class="selected-dish-head-left">
-                 <div class="selected-dish-count">${shoppingCartProducts[i].itemAmount}</div>
-                 <div class="selected-dish-name">${shoppingCartProducts[i].itemName}</div>
-              </div>
-              <div class="selected-dish-price"><span>${shoppingCartProducts[i].itemPrice}</span>€</div>
-           </div>
-           <div class="selected-dish-bottom">
-              <div class="selected-dish-addcomment">Anmerkung hinzufügen</div>
-              <div class="selected-dish-left-counter">
-                 <div class="selected-dish-counter-icon">
-                    <img src="./src/img/remove_icon.png" alt="remove icon" onclick="removeItemFromShoppingCart(${i})"/>
-                 </div>
-                 <div class="selected-dish-price"><span>${shoppingCartProducts[i].itemAmount}</span></div>
-                 <div class="selected-dish-counter-icon"><img src="./src/img/add_icon.png" alt="add icon" onclick="increaseQuantityFromItem(${i})"/></div>
-              </div>
-           </div>
-        `;
+      renderShoppingCartMobileHTML(i);
    }
 }
 
